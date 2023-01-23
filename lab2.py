7e132270dadcc0d37d64ea87d78f73969020bab7
Mukhammedali Zhursinbek <122848473+justfable@users.noreply.github.com>
2023-01-24 04:41:19 +0600

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -4,7 +4,7 @@ print("2+2=4?        1-yes, 0-no")
 print("Ваш выбор:")
 n = int(input())
 if n == 0:
-    print("Попробуйте еще раз!")
+    print("Попробуйте завтра!")
 elif n == 1:
     print("Вы гений!")
 else:
