1c522ad69aff2fef40a5b7e7d19df1ce6cf7dabf
Mukhammedali Zhursinbek <122848473+justfable@users.noreply.github.com>
2023-01-24 04:35:03 +0600

    Add files via upload

---
+++ lab2.py
@@ -0,0 +1,15 @@
+print("Programma studenta 3-go kursa SU - Zhursinbek Mukhammedali")
+print("---------------------------------------------------")
+print("2+2=4?        1-yes, 0-no")
+print("Ваш выбор:")
+n = int(input())
+if n == 0:
+    print("Попробуйте еще раз!")
+elif n == 1:
+    print("Вы гений!")
+else:
+    print("Вы на шаге к успеху!")
+
+#Программа для определения I-Q
+
+
